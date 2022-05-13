83ef55cc92d0024fe393210a295ab12321516674
Plamen Ivanov <paco.iwanow@gmail.com>
2022-05-13 19:55:58 +0300

    fix-typo

--- push.sh
+++ push.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-if [ $# -ne 2 ];
-then
-    echo -e '+++ERROR+++\n->> missing parameters:\n1)==> directory\n2)==> commit message'
+if [ $# -ne 2 ]; then
+  echo -e '+++ERROR+++\n->> missing parameters:\n1)==> directory\n2)==> commit message'
   exit 1
 fi
 
 cd $1
 git add .
-git commit -m '$($2)'
-git push origin main
-
+git commit -m $2
+git push origin master
